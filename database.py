58a4f524451deb97a462fec90634638f18dd28f5
B1ackEx_ <binw1736@gmail.com>
2026-06-13 11:39:43 +0500

    hotel booking project completed

---
+++ database.py
@@ -0,0 +1,15 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv(dotenv_path=".env")  # FORCE correct path
+
+MONGO_URL = os.getenv("MONGO_URL")
+
+if not MONGO_URL:
+    raise Exception("MONGO_URL not found - check .env file")
+
+client = AsyncIOMotorClient(MONGO_URL)
+
+db = client.hotel_booking_db
+bookings_collection = db.bookings
---
+++ main.py
@@ -0,0 +1,51 @@
+from fastapi import FastAPI, Form
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
+from database import bookings_collection
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+# HOME PAGE
+@app.get("/")
+def home():
+    return FileResponse("static/index.html")
+
+
+# BOOKING API
+@app.post("/book")
+async def book(
+    full_name: str = Form(...),
+    email: str = Form(...),
+    phone: str = Form(...),
+    check_in: str = Form(...),
+    check_out: str = Form(...),
+    room_type: str = Form(...),
+    guests: int = Form(...)
+):
+
+    booking = {
+        "full_name": full_name,
+        "email": email,
+        "phone": phone,
+        "check_in": check_in,
+        "check_out": check_out,
+        "room_type": room_type,
+        "guests": guests
+    }
+
+    result = await bookings_collection.insert_one(booking)
+
+    return {
+        "message": "Booking Successful",
+        "booking_id": str(result.inserted_id)
+    }
+
+import os
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
