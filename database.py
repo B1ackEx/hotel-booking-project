edc137407fe312de1f0b595d2162b4d1d5a399d9
B1ackEx_ <binw1736@gmail.com>
2026-06-13 12:11:39 +0500

    Fix DB

--- database.py
+++ database.py
@@ -9,7 +9,11 @@ MONGO_URL = os.getenv("MONGO_URL")
 if not MONGO_URL:
     raise Exception("MONGO_URL not found - check .env file")
 
-client = AsyncIOMotorClient(MONGO_URL)
+client = AsyncIOMotorClient(
+    MONGO_URL,
+    tls=True,
+    tlsAllowInvalidCertificates=True
+)
 
 db = client.hotel_booking_db
 bookings_collection = db.bookings
